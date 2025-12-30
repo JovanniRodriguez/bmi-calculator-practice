e7712a0dbbd4c72c548c926b4d3b569004d9b4dc
jovyp <jovips1@gmail.com>
2025-12-30 15:49:47 -0500

    Learned how to connect .xml Views to MainActivity.java and turn them into usable objects to modify

--- app/src/main/java/com/jovannirodriguez/bmicalculator/MainActivity.java
+++ app/src/main/java/com/jovannirodriguez/bmicalculator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.jovannirodriguez.bmicalculator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +15,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //app entry point
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); //reload state data
+        setContentView(R.layout.activity_main); //set default "screen"/.xml file
+
+
+        // grab all the View objects we need to interact with from the layout file
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        RadioButton maleButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleButton = findViewById(R.id.radio_button_female);
+
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText feetEditText = findViewById(R.id.edit_text_feet);
+        EditText inchesEditText = findViewById(R.id.edit_text_inches);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+
+
     }
 }
